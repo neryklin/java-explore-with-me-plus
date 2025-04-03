6ba0a6ab10b8c818f31d8f0993095730cbe4c2c1
yandere <nusvincer@gmail.com>
2025-04-03 16:31:28 +0400

    Реализация StatsClient

--- stats/stats-client/src/main/java/ru/practicum/stats_client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/stats_client/StatsClient.java
@@ -2,14 +2,14 @@ package ru.practicum.stats_client;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.stats_dto.CreateHitDto;
+import ru.practicum.stats_dto.CreateStatsDto;
 import ru.practicum.stats_dto.ResponseStatsDto;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -25,15 +25,20 @@ public class StatsClient {
         restTemplate.postForEntity(baseUrl + "/hit", hitDto, Void.class);
     }
 
-    public List<ResponseStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+    public List<ResponseStatsDto> getStats(CreateStatsDto statsDto) {
         UriComponentsBuilder builder = UriComponentsBuilder
                 .fromHttpUrl(baseUrl + "/stats")
-                .queryParam("start", start)
-                .queryParam("end", end)
-                .queryParam("unique", unique);
+                .queryParam("start", statsDto.getStart())
+                .queryParam("end", statsDto.getEnd());
 
-        for (String uri : uris) {
-            builder.queryParam("uris", uri);
+        if (statsDto.getUris() != null) {
+            for (String uri : statsDto.getUris()) {
+                builder.queryParam("uris", uri);
+            }
+        }
+
+        if (Boolean.TRUE.equals(statsDto.getUnique())) {
+            builder.queryParam("unique", true);
         }
 
         ResponseEntity<ResponseStatsDto[]> response = restTemplate.getForEntity(
