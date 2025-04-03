12972504108fe3b4b7c655fdcb966257e1b72669
neryklin <neryklin@mail.ru>
2025-04-03 23:13:00 +0300

    поправленые замечания по структуре

--- stats/stats-dto/src/main/java/ru/practicum/stats_dto/CreateHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats_dto/CreateHitDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.stats_dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -17,13 +18,15 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreateHitDto {
     long id;
-
+    @NotEmpty
     @Size(max = 255)
     String app;
 
+    @NotEmpty
     @Size(max = 255)
     String uri;
 
+    @NotEmpty
     @Size(max = 255)
     String ip;
 
