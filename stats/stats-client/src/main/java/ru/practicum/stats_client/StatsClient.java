fd0655aa7f87f0fa3f78364b67618e7a79a59de1
yandere <nusvincer@gmail.com>
2025-04-04 15:12:43 +0400

    Адаптация под WebClient

--- stats/stats-client/src/main/java/ru/practicum/stats_client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/stats_client/StatsClient.java
@@ -1,51 +1,14 @@
 package ru.practicum.stats_client;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.stats_dto.CreateHitDto;
 import ru.practicum.stats_dto.CreateStatsDto;
 import ru.practicum.stats_dto.ResponseStatsDto;
 
 import java.util.List;
 
-@Component
-@RequiredArgsConstructor
-public class StatsClient implements StatsClientInterface {
+public interface StatsClient {
 
-    private final RestTemplate restTemplate;
+    void sendHit(CreateHitDto hitDto);
 
-    @Value("${stats-server.url:http://localhost:9090}")
-    private String baseUrl;
-
-    @Override
-    public void sendHit(CreateHitDto hitDto) {
-        restTemplate.postForEntity(baseUrl + "/hit", hitDto, Void.class);
-    }
-
-    @Override
-    public List<ResponseStatsDto> getStats(CreateStatsDto statsDto) {
-        UriComponentsBuilder builder = UriComponentsBuilder
-                .fromHttpUrl(baseUrl + "/stats")
-                .queryParam("start", statsDto.getStart())
-                .queryParam("end", statsDto.getEnd());
-
-        if (statsDto.getUris() != null) {
-            for (String uri : statsDto.getUris()) {
-                builder.queryParam("uris", uri);
-            }
-        }
-
-        if (Boolean.TRUE.equals(statsDto.getUnique())) {
-            builder.queryParam("unique", true);
-        }
-
-        ResponseEntity<ResponseStatsDto[]> response = restTemplate.getForEntity(
-                builder.toUriString(), ResponseStatsDto[].class);
-
-        return List.of(response.getBody());
-    }
+    List<ResponseStatsDto> getStats(CreateStatsDto statsDto);
 }
