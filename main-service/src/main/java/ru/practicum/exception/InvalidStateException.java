795a9d1d37ece996b52df172c57da9bf2a30e8aa
Pavel Usoltsev <usoltsevdisk@yandex.ru>
2025-04-13 19:16:48 +0300

    Ошибки стиля

--- main-service/src/main/java/ru/practicum/exception/InvalidStateException.java
+++ main-service/src/main/java/ru/practicum/exception/InvalidStateException.java
@@ -1,5 +1,7 @@
 package ru.practicum.exception;
 
 public class InvalidStateException extends RuntimeException {
-    public InvalidStateException(String message) { super(message); }
+    public InvalidStateException(String message) {
+        super(message);
+    }
 }
